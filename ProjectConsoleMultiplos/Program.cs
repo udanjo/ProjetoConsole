bc9c687708bbf3e9ebeab1acac4dd363f069053d
ueverson diego soares pedroso <udanjo_hyuuga@hotmail.com>
2021-07-01 22:23:58 -0300

    feat: Projeto 3 Pronto

---
+++ ProjectConsoleMultiplos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectConsoleMultiplos
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            IList<int> list = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                if ((i % 3) == 0) list.Add(i);
+            }
+
+            var values = string.Join(",", list);
+            Console.WriteLine("Os números múltiplos de 3 de 1 a 100, são:");
+            Console.WriteLine(values);
+            Console.ReadLine();
+        }
+    }
+}
