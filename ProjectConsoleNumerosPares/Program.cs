abbfb60d84be3ae61a4d59248a2af823e25f8948
ueverson diego soares pedroso <udanjo_hyuuga@hotmail.com>
2021-09-09 16:38:07 -0300

    chore: Console 7

---
+++ ProjectConsoleNumerosPares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProjectConsoleNumerosPares
+{
+    public class Program
+    {
+        private const int _countNumber = 4;
+
+        private static void Main()
+        {
+            int i = 1, totalPairs = 0;
+            while (i <= _countNumber)
+            {
+                int value;
+                do
+                {
+                    Console.WriteLine("Informe um numero:");
+                } while (!int.TryParse(Console.ReadLine(), out value));
+
+                totalPairs += value % 2 == 0 ? value : 0;
+                i++;
+            }
+
+            Console.WriteLine($"\nValor total dos numeros pares é: {totalPairs}");
+            Console.ReadKey();
+        }
+    }
+}
